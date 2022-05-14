e1e26d8538b02f0b74119734c705b099c8d6f5c4
Zack Project <zackprojectapp@gmail.com>
2022-05-14 15:51:43 +0200

    change generations

--- higherlower/index.js
+++ higherlower/index.js
@@ -3,40 +3,87 @@ var poke1;
 var poke2;
 var score = 0;
 var maxScore = 0;
+var type = 'speed';
+var gen = '1';
+
+function changeGenOrType() {
+    let typeHtml = document.getElementById('typeViewList').selectedOptions[0].value;
+    let genHtml = document.getElementById('genViewList').selectedOptions[0].value;
+    type = typeHtml;
+    gen = parseInt(genHtml);
+    score = 0;
+    maxScore = 0;
+    refreshScore();
+    loadTwoPokes();
+}
+
 
 function loadTwoPokes() {
+    //An array of 2 position can be asigned on two variables
+    let listGen = genMaxMin(gen);
+    let min = listGen[0];
+    let max = listGen[1];
     //Sellect two random pokes
-    poke1 = data[randNumber(898, 1)];
-    poke2 = data[randNumber(898, 1)];
+    poke1 = data[randNumber(max, min)];
+    poke2 = data[randNumber(max, min)];
     printPokeHTML();
 }
 
 function twiceNewPoke() {
+    //An array of 2 position can be asigned on two variables
+    let listGen = genMaxMin(gen);
+    let min = listGen[0];
+    let max = listGen[1];
     //Copy the first poke in the left and new random to right
     poke1 = poke2;
-    poke2 = data[randNumber(898, 1)];
+    poke2 = data[randNumber(max, min)];
     printPokeHTML();
 }
 
+//retun max min for generation in dex number
+function genMaxMin(genl) {
+    //return de range of number pokedex for each generation
+    switch (genl) {
+        case 1:
+            return [1, 151]; //gen1
+        case 2:
+            return [152, 251]; //gen2
+        case 3:
+            return [252, 386]; //gen3
+        case 4:
+            return [387, 493]; //gen4
+        case 5:
+            return [494, 649]; //gen5
+        case 6:
+            return [650, 721]; //gen6
+        case 7:
+            return [722, 809]; //gen7
+        case 1:
+            return [810, 898]; //gen8
+        default:
+            return [1, 898]; //all gens gen=0 or other
+    }
+
+}
 function printPokeHTML() {
     // First poke
     document.getElementById('pk1name').textContent = poke1.name;
     document.getElementById('pk1image').src = poke1.img_default;
-    document.getElementById('pk1number').textContent = poke1.speed;
-    document.getElementsByClassName('pktpye')[0].textContent = 'speed';
+    document.getElementById('pk1number').textContent = poke1[type]; //Ex: type['speed'] =  156
+    document.getElementsByClassName('pktpye')[0].textContent = type; //Ex: type = 'speed'
     //second poke
     document.getElementById('pk2name').textContent = poke2.name;
     document.getElementById('pk2image').src = poke2.img_default;
-    // document.getElementById('pk2number').textContent = poke2.speed; THIS NUMBER IS SECRET
-    document.getElementsByClassName('pktpye')[1].textContent = 'speed';
+    document.getElementsByClassName('pktpye')[1].textContent = type;
+    // document.getElementById('pk2number').textContent = poke2[type]; THIS NUMBER IS SECRET
 }
 
 function btnLow() {
-    if (poke1.speed < poke2.speed) {
+    if (poke1[type] < poke2[type]) {
         score++;
     } else {
         score = 0;
-        alert(`WRONG!!!\n${poke1.name} : ${poke1.speed}\n${poke2.name} : ${poke2.speed}\n`)
+        alert(`WRONG!!!\n${poke1.name} ${type} : ${poke1[type]}\n${poke2.name} ${type}: ${poke2[type]}\n`)
     }
 
     //Refresh the score in the html and max score
@@ -45,11 +92,11 @@ function btnLow() {
 }
 
 function btnHigh() {
-    if (poke1.speed > poke2.speed) {
+    if (poke1[type] > poke2[type]) {
         score++;
     } else {
         score = 0;
-        alert(`WRONG!!!\n${poke1.name} : ${poke1.speed}\n${poke2.name} : ${poke2.speed}\n`)
+        alert(`WRONG!!!\n${poke1.name} : ${poke1[type]}\n${poke2.name} : ${poke2[type]}\n`)
     }
     refreshScore();
     twiceNewPoke();
@@ -64,8 +111,4 @@ function refreshScore() {
 
 function randNumber(max, min) {
     return Math.floor(Math.random() * (max - min) + min);
-}
-
-function consoleThis() {
-    console.log("hola");
 }
