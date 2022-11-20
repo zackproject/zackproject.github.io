adf75114b6b5f12ca3e1744dcebdb2a7277721f7
dev <44408822+zackproject@users.noreply.github.com>
2022-11-20 21:24:46 +0100

    Calendar christmas (#4)
    * Add Christmas Calendar
    
    * Linkear navidad

---
+++ navidad/dades.js
@@ -0,0 +1,373 @@
+var adventList = [
+    {
+        "id": 1,
+        "image_open": "images/resolved/resolved_1.webp",
+        "advent": "https://youtu.be/rnS-05XoXs4"
+    },
+    {
+        "id": 2,
+        "image_open": "images/resolved/resolved_2.webp",
+        "advent": "https://youtu.be/VS-Jk2YwA0E"
+    },
+    {
+        "id": 3,
+        "image_open": "images/resolved/resolved_4.webp",
+        "advent": "https://youtu.be/Eq0J2W7vkDQ"
+    },
+    {
+        "id": 4,
+        "image_open": "images/resolved/resolved_3.webp",
+        "advent": "https://youtu.be/rBQEW5uFdDY"
+    },
+    {
+        "id": 5,
+        "image_open": "images/resolved/resolved_5.webp",
+        "advent": "https://youtu.be/XeM0hGdzVzQ"
+    },
+    {
+        "id": 6,
+        "image_open": "images/resolved/resolved_6.webp",
+        "advent": "https://youtu.be/rVsP8nm5hiE"
+    },
+    {
+        "id": 7,
+        "image_open": "images/resolved/resolved_7.webp",
+        "advent": "https://youtu.be/TnKyvgoO_VI"
+    },
+    {
+        "id": 8,
+        "image_open": "images/resolved/resolved_8.webp",
+        "advent": "https://youtu.be/R2mZ64XAmqw"
+    },
+    {
+        "id": 9,
+        "image_open": "images/resolved/resolved_9.webp",
+        "advent": "https://youtu.be/kEPfM3jSoBw"
+    },
+    {
+        "id": 10,
+        "image_open": "images/resolved/resolved_10.webp",
+        "advent": "https://youtu.be/INkzB4iJrnY"
+    },
+    {
+        "id": 11,
+        "image_open": "images/resolved/resolved_11.webp",
+        "advent": "https://youtube.com/shorts/JL2A6rNgOns"
+    },
+    {
+        "id": 12,
+        "image_open": "images/resolved/resolved_12.webp",
+        "advent": "https://youtu.be/cG8o24djj-o"
+    },
+    {
+        "id": 13,
+        "image_open": "images/resolved/resolved_13.webp",
+        "advent": "https://youtu.be/VZzSBv6tXMw"
+    },
+    {
+        "id": 14,
+        "image_open": "images/resolved/resolved_14.webp",
+        "advent": "https://youtu.be/ILZh2xncU8A"
+    },
+    {
+        "id": 15,
+        "image_open": "images/resolved/resolved_15.webp",
+        "advent": "https://youtu.be/Nwnyri1BvTk"
+    },
+    {
+        "id": 16,
+        "image_open": "images/resolved/resolved_16.webp",
+        "advent": "https://youtu.be/9klzZsVw-cQ"
+    },
+    {
+        "id": 17,
+        "image_open": "images/resolved/resolved_17.webp",
+        "advent": "https://youtu.be/zNM8eV9tcK0"
+    },
+    {
+        "id": 18,
+        "image_open": "images/resolved/resolved_18.webp",
+        "advent": "https://youtu.be/MLy655j9LM8"
+    },
+    {
+        "id": 19,
+        "image_open": "images/resolved/resolved_19.webp",
+        "advent": "https://youtu.be/yV0Nf6NqMv0"
+    },
+    {
+        "id": 20,
+        "image_open": "images/resolved/resolved_20.webp",
+        "advent": "https://youtu.be/WCu00OVc-Yc"
+    },
+    {
+        "id": 21,
+        "image_open": "images/resolved/resolved_21.webp",
+        "advent": "https://youtu.be/XwGFQsyi8PE"
+    },
+    {
+        "id": 22,
+        "image_open": "images/resolved/resolved_22.webp",
+        "advent": "https://google.ca/maecenas/tincidunt/lacus.jpg"
+    },
+    {
+        "id": 23,
+        "image_open": "images/resolved/resolved_23.webp",
+        "advent": "https://youtu.be/50iY3RJm898"
+    },
+    {
+        "id": 24,
+        "image_open": "images/resolved/resolved_24.webp",
+        "advent": "https://youtu.be/Pk1I8uEMrP8"
+    }
+]
+
+
+var resolveList = [2, 0, 1, 3, 0, 0, 2, 1, 2, 3, 1, 1, 1, 2, 2, 3, 3, 0, 0, 1, 1, 2, 3, 1]
+var quizList = [
+    {
+        "id": 1,
+        "question": "Cual es el Pokémon insignia de Ash",
+        "options": [
+            "Vaporeon Blanco",
+            "Sentret",
+            "Pikachu",
+            "Turtwig"
+        ]
+    },
+    {
+        "id": 2,
+        "question": "Ibai dijo: Toxicidad fuera",
+        "options": [
+            "Mala Vibra Fuera",
+            "Espejo Magico",
+            "Acero",
+            "Mala vibra dentro"
+        ]
+    },
+    {
+        "id": 3,
+        "question": "Que significa 'Ohana'",
+        "options": [
+            "Poder",
+            "Familia",
+            "Furro",
+            "Amistad"
+        ]
+    },
+    {
+        "id": 4,
+        "question": "Cual es el quirk de Deku",
+        "options": [
+            "El poder del Guión",
+            "All for One",
+            "Brocoli",
+            "One For All",
+        ]
+    },
+    {
+        "id": 5,
+        "question": "Cual NO fue un fantasma de la Navidad",
+        "options": [
+            "Multiversal",
+            "Presente",
+            "Pasado",
+            "Futuro"
+        ]
+    },
+    {
+        "id": 6,
+        "question": "Cual es la funcion del globulo blanco",
+        "options": [
+            "Combatir infecciones y enfermedades",
+            "Transportar oxigeno",
+            "Ayudar a coagular sangre en caso de herida",
+            "Causar alergias"
+        ]
+    },
+    {
+        "id": 7,
+        "question": "Según la canción, cuanto es '1+1'",
+        "options": [
+            "Son 1",
+            "Son 2",
+            "Son 7",
+            "El fantastico Ralph"
+        ]
+    },
+    {
+        "id": 8,
+        "question": "Como se llama el mono de Dora la Exploradora",
+        "options": [
+            "Jorge el Curioso",
+            "Botas",
+            "Sin Nombre",
+            "Mono Guay"
+        ]
+    },
+
+    {
+        "id": 9,
+        "question": "Por qué se hundió el Titanic",
+        "options": [
+            "Por tonto",
+            "Por no abrigarse",
+            "Por un Iceberg",
+            "No se hundió"
+        ]
+    },
+    {
+        "id": 10,
+        "question": "Según la 1r Película, cuantos Dálmatas eran",
+        "options": [
+            "4",
+            "102",
+            "100",
+            "101"
+        ]
+    },
+    {
+        "id": 11,
+        "question": "I say Disco you say",
+        "options": [
+            "Disco",
+            "Party",
+            "Discoteca",
+            "Holiday"
+        ]
+    },
+    {
+        "id": 12,
+        "question": "Como se llama la punta del cordón",
+        "options": [
+            "Plastidor/Varillita",
+            "Herrete/Acetato",
+            "Cilindro",
+            "Mamihlapinatapai"
+        ]
+    },
+    {
+        "id": 13,
+        "question": "Rubius decia: Su su suscribete",
+        "options": [
+            "Y manita arriba si te gustó",
+            "Y dale a like si quieres",
+            "Y uwu",
+            "Y dale me gusta"
+        ]
+    },
+    {
+        "id": 14,
+        "question": "Que poder tiene Eren Jaegger",
+        "options": [
+            "El titan bestia",
+            "El titan mandibula",
+            "El titan de ataque",
+            "El titan colosal"
+        ]
+    },
+    {
+        "id": 15,
+        "question": "Que es el One Piece",
+        "options": [
+            "Un tesoro",
+            "La amistad",
+            "No se sabe oficialmente",
+            "Sogeking"
+        ]
+    },
+    {
+        "id": 16,
+        "question": "Cual es el número del protagonista en El Juego de Calamar",
+        "options": [
+            "0",
+            "1",
+            "267",
+            "456"
+        ]
+    },
+    {
+        "id": 17,
+        "question": "Que territorio SI existe",
+        "options": [
+            "Narnia",
+            "El pais de Nunca Jamás",
+            "Mordor",
+            "Murcia"
+        ]
+    },
+    {
+        "id": 18,
+        "question": "Que actor NO interpretó a Spiderman",
+        "options": [
+            "Math Smith",
+            "Tom Holland",
+            "Tobey Maguire",
+            "Andrew Garfield"
+        ]
+    },
+    {
+        "id": 19,
+        "question": "Según la película, de que era la fábrica de Charlie",
+        "options": [
+            "Chocolate",
+            "Drogas",
+            "Golosinas",
+            "Vino"
+        ]
+    },
+    {
+        "id": 20,
+        "question": "Quién es Hatsune Miku",
+        "options": [
+            "Youtuber",
+            "Cantante virtual",
+            "Albañil",
+            "Senadora"
+        ]
+    },
+    {
+        "id": 21,
+        "question": "Quién es Ryuk de Death Note",
+        "options": [
+            "Hada",
+            "Shinigami ",
+            "Mi novio",
+            "Espiritu"
+        ]
+    },
+    {
+        "id": 22,
+        "question": "De que aldea oculta es Naruto",
+        "options": [
+            "Sonido",
+            "Niebla",
+            "Hoja",
+            "Lluvia"
+        ]
+    },
+    {
+        "id": 23,
+        "question": "Que es una JoJo Referencia?",
+        "options": [
+            "Respirar",
+            "Caminar",
+            "Luchar",
+            "Todo es una JoJo Referencia"
+        ]
+    },
+    {
+        "id": 24,
+        "question": "Cual NO es un emoticono escrito",
+        "options": [
+            "UwU",
+            "PH",
+            "XD",
+            "OwO"
+        ]
+    }
+]
+/*
+for (let index = 0; index < quizList.length; index++) {
+    const element = quizList[index];
+    console.log(index + 1, element.question);
+}*/
