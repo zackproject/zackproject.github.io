90f0b8ca76ef3d9c9b86cf86857ebfd69943d5e1
zackproject <zackprojectapp@gmail.com>
2023-03-28 22:36:52 +0200

    flip unique card

---
+++ quien-es-quien/object.js
@@ -0,0 +1,67 @@
+let quienEsQuien = [
+    {
+        "name": "My Hero Academia",
+        "characters": [
+            {
+                "id": 1,
+                "name": "Izuku Midoriya",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/2/25/Izuku_Midoriya_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619050117"
+            },
+            {
+                "id": 2,
+                "name": "Katsuki Bakugo",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/c/cd/Katsuki_Bakugo_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619050033"
+            },
+            {
+                "id": 3,
+                "name": "Ochaco Uraraka",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/7/78/Ochaco_Uraraka_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619044306"
+            },
+            {
+                "id": 4,
+                "name": "Tenya Iida",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/c/ca/Tenya_Ida_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619044125"
+            },
+            {
+                "id": 5,
+                "name": "Shoto Todoroki",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/7/7f/Shoto_Todoroki_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619045658"
+            },
+            {
+                "id": 6,
+                "name": "Tsuyu Asui",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/7/7c/Tsuyu_Asui_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619043915"
+            },
+            {
+                "id": 7,
+                "name": "Eijiro Kirishima",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/c/ca/Tenya_Ida_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619044125"
+            },
+            {
+                "id": 8,
+                "name": "Momo Yaoyorozu",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/8/8d/Momo_Yaoyorozu_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619050232"
+            },
+            {
+                "id": 9,
+                "name": "Denki Kaminari",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/b/b0/Denki_Kaminari_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619044711"
+            },
+            {
+                "id": 10,
+                "name": "Kyoka Jiro",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/3/32/Kyoka_Jiro_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619045418"
+            },
+            {
+                "id": 11,
+                "name": "Minoru Mineta",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/6/6a/Minoru_Mineta_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619050156"
+            },
+            {
+                "id": 12,
+                "name": "Fumikage Tokoyami",
+                "image": "https://static.wikia.nocookie.net/bokunoheroacademia/images/6/6c/Fumikage_Tokoyami_Portrait.png/revision/latest/scale-to-width-down/135?cb=20160619045620"
+            }
+        ]
+    }
+]
