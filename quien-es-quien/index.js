430a98e9abc74fe33796c9c7475cb25fbe2f3b81
zackproject <zackprojectapp@gmail.com>
2023-03-31 17:24:50 +0200

    Building selector

--- quien-es-quien/index.js
+++ quien-es-quien/index.js
@@ -1,3 +1,7 @@
+var quienEsQuienList = quienEsQuien[0].characters;
+var ntitle = quienEsQuien[0].title;
+var selectedCharacter = 0;
+
 //https://www.w3schools.com/howto/howto_css_flip_card.asp
 function flipCard(event) {
     let ncard = document.getElementsByClassName("flip-card-inner");
@@ -19,6 +23,8 @@ function flipCard(event) {
     if (pos !== null) {
         ncard[pos].style.transform = ncard[pos].style.transform ? "" : "rotateY(180deg)"
     }
+    console.log("clicado", event.target.id);
+
 
 }
 
@@ -30,34 +36,61 @@ function flipAllCard() {
     }
 }
 
-function cardHTML(nid, img, name, anime) {
+function cardHTML(props) {
     return `<div class="card">
     <div class="flip-card" onclick="flipCard(event)">
-        <div id="card-${nid}" class="flip-card-inner">
+        <div id="card-${props.id}" class="flip-card-inner">
             <div class="flip-card-front">
-                <img id="img-${nid}" src="${img}" alt="Avatar" height="100%" width="100%">
+                <img id="img-${props.id}" src="${props.image}" alt="Avatar" height="100%" width="100%">
             </div>
             <div class="flip-card-back">
-                <h1>${name}</h1>
-                <p>${anime}</p>
+                <h1 id="name-${props.id}">${props.name}</h1>
+                <p id="anime-${props.id}">${props.anime}</p>
             </div>
         </div>
     </div>
 </div>`
 }
+
+function addSelectorCharacter(selected) {
+    document.getElementById("image-card").src = selected[0].image;
+}
+
+function nextImage() {
+    if (selectedCharacter < quienEsQuienList.length) {
+        selectedCharacter++;
+    }
+    document.getElementById("image-card").src = quienEsQuienList[selectedCharacter].image;
+    console.log("next", quienEsQuienList[selectedCharacter].image);
+}
+function backImage() {
+    if (selectedCharacter > quienEsQuienList.length) {
+        selectedCharacter--;
+    }
+    document.getElementById("image-card").src = quienEsQuienList[selectedCharacter].image;
+    console.log("next", quienEsQuienList[selectedCharacter].image);}
+
+function select() {
+
+}
+
+
 function novaPartida() {
-    let ntitle = quienEsQuien[0].title;
-    quienEsQuien[0].characters.forEach(e => {
-        document.getElementById("cards").innerHTML += cardHTML(e.id, e.image, e.name, ntitle)
-    
+    quienEsQuienList.forEach(e => {
+        console.log(ntitle);
+        let props = { id: e.id, image: e.image, name: e.name, anime: ntitle };
+        document.getElementById("cards").innerHTML += cardHTML(props)
+
     });
 
     for (let i = 0; i < document.getElementsByClassName("card").length; i++) {
         const element = document.getElementsByClassName("card")[i];
         element.style.animation = "aparece 2s";
-        element.style.animationDelay = i/4 + "s";
+        element.style.animationDelay = i / 4 + "s";
         element.style.animationFillMode = "forwards";
     }
+
+    addSelectorCharacter(quienEsQuien[0].characters);
 }
 
 
