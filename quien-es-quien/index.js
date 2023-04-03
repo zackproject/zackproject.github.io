1fb5ebf2dcb00d86646617e27d98a0c323ecf057
zackproject <zackprojectapp@gmail.com>
2023-04-03 03:34:50 +0200

    logica OK

--- quien-es-quien/index.js
+++ quien-es-quien/index.js
@@ -1,14 +1,72 @@
+//Objecte que controla el joc 'Quien es quien'
+class WhoIsWho {
+    constructor(animeName, characterList, maxCharacters, myCharacter) {
+        this.animeName = animeName;
+        this.characterList = characterList
+        this.maxCharacters = maxCharacters;
+        this.myCharacter = myCharacter;
+    }
 
-//Anime selecionat
-var animeId = 0;
-//Personatge per jugar seleccionat
-var selectedCharacter = 0;
-//Total Carta
-var maxCard = 25;
-//Personatges del anime
-var quienEsQuienList = quienEsQuien[animeId].characters;
-//Titol
-var ntitle = quienEsQuien[animeId].title;
+    //Actualitza el personatge del Player
+    nextCharacter() {
+        //No guarda la llargaria, guarda el id de l'ultim de la llista
+        if (this.myCharacter.id < this.characterList[this.characterList.length - 1].id) {
+            //Dins els compara la id del 1r element i la id del ultim element
+            this.myCharacter = this.characterList[this.myCharacter.id + 1];
+        }
+    }
+    //Actualitza el personatge del Player
+    backCharacter() {
+        if (this.myCharacter.id > this.characterList[0].id) {
+            this.myCharacter = this.characterList[this.myCharacter.id - 1];
+        }
+    }
+
+}
+
+console.log("search", document.location.search);
+params = new URLSearchParams(document.location.search);
+//console.log(params.get("name"));
+
+params.forEach(element => {
+    console.log(element);
+});
+
+
+//Partida per defecte
+var player = new WhoIsWho(
+    quienEsQuien[0].title, //Nom de la serie
+    quienEsQuien[0].characters, //Personatges de Shingeki
+    quienEsQuien[0].characters.length, // Maxim de personatge en partida
+    quienEsQuien[0].characters[0]); // Personatge triat per defecte
+
+console.log(player);
+//Objecte del joc
+
+
+function generateSelects() {
+    //Selector d'anime
+    let selectAnime = document.getElementById("select-anime");
+    for (i in quienEsQuien) {
+        let noption = document.createElement("option");
+        //Seleciona el primer item
+        if (i == 0) noption.selected = true;
+        //El value es la i
+        noption.value = i;
+        //El text son els anime
+        noption.text = quienEsQuien[i].title;
+        selectAnime.appendChild(noption)
+    }
+
+    //Omple l'ultim selector de personatges, el primer son '0' i '12' (html)
+    let selectCards = document.getElementById("select-cards");
+    let moption = document.createElement("option");
+    moption.value = player.maxCharacters;
+    //El text son els changeAnime
+    moption.selected = "true";
+    moption.text = player.maxCharacters + " personajes";
+    selectCards.appendChild(moption)
+}
 
 //https://www.w3schools.com/howto/howto_css_flip_card.asp
 function flipCard(event) {
@@ -31,9 +89,6 @@ function flipCard(event) {
     if (pos !== null) {
         ncard[pos].style.transform = ncard[pos].style.transform ? "" : "rotateY(180deg)"
     }
-    console.log("clicado", event.target.id);
-
-
 }
 
 function flipAllCard() {
@@ -49,7 +104,7 @@ function cardHTML(props) {
     <div class="flip-card" onclick="flipCard(event)">
         <div id="card-${props.id}" class="flip-card-inner">
             <div class="flip-card-front">
-                <img id="img-${props.id}" src="${props.image}" alt="Avatar" height="100%" width="100%">
+                <img id="img-${props.id}" src="${props.image}" alt="${props.name}" height="100%" width="100%">
             </div>
             <div class="flip-card-back">
                 <h1 id="name-${props.id}">${props.name}</h1>
@@ -63,23 +118,20 @@ function cardHTML(props) {
 
 
 function nextImage() {
-    if (selectedCharacter < quienEsQuienList.length - 1) {
-        selectedCharacter++;
-        document.getElementById("image-card").src = quienEsQuienList[selectedCharacter].image;
-    }
+    player.nextCharacter();
     refreshViewSelector()
 }
 function backImage() {
-    if (selectedCharacter > 0) {
-        selectedCharacter--;
-        document.getElementById("image-card").src = quienEsQuienList[selectedCharacter].image;
-    }
+    player.backCharacter();
     refreshViewSelector();
 }
 
 function refreshViewSelector() {
-    document.getElementById("indexSelect").innerText = selectedCharacter + 1;
-    document.getElementById("indexMax").innerText = quienEsQuienList.length;
+    document.getElementById("image-name").innerText = player.myCharacter.name;
+    document.getElementById("image-card").src = player.myCharacter.image;
+    document.getElementById("indexSelect").innerText = parseInt(player.myCharacter.id) + 1;
+    document.getElementById("indexMax").innerText = player.maxCharacters;
+
 }
 
 function deleteChilds(currentDiv) {
@@ -92,10 +144,9 @@ function deleteChilds(currentDiv) {
 function novaPartida() {
     deleteChilds(document.getElementById("cards"));
     let cont = 0;
-    quienEsQuienList.forEach(e => {
-        if (cont < maxCard) {
-            console.log(ntitle);
-            let props = { id: e.id, image: e.image, name: e.name, anime: ntitle };
+    player.characterList.forEach(e => {
+        if (cont < player.maxCharacters) {
+            let props = { id: e.id, image: e.image, name: e.name, anime: player.animeName };
             document.getElementById("cards").innerHTML += cardHTML(props)
             cont++;
         }
@@ -108,39 +159,36 @@ function novaPartida() {
         element.style.animationFillMode = "forwards";
     }
 
-    document.getElementById("image-card").src = quienEsQuienList[selectedCharacter].image;
+    document.getElementById("image-card").src = player.myCharacter.image;
 
 }
+
 
 
 function loadGame() {
     //Posa la imatge per defecte del personatge
-    document.getElementById("image-card").src = quienEsQuienList[selectedCharacter].image;
+    document.getElementById("image-card").src = player.myCharacter.image;
     //Omple el selector de anime
-    let selectAnime = document.getElementById("select-anime");
-    for (i in quienEsQuien) {
-        let noption = document.createElement("option");
-        //Seleciona el primer item
-        if (i == 0) noption.selected = true;
-        //El value es la i
-        noption.value = i;
-        //El text son els anime
-        noption.text = quienEsQuien[i].title;
-        selectAnime.appendChild(noption)
-    }
-
-    //Omple l'ultim selector de personatges, el primer son '0' i '12' (html)
-    let selectCards = document.getElementById("select-cards");
-    let moption = document.createElement("option");
-    moption.value = quienEsQuienList.length;
-    //El text son els anime
-    moption.selected = "true";
-    moption.text = quienEsQuienList.length + " personajes";
-    selectCards.appendChild(moption)
+    generateSelects();
+    //Refresa la vista
     refreshViewSelector();
 }
 
-function changeCharacter (){
-    let a = document.getElementById("select-cards").selectedOptions[0].value;
-    maxCard = parseInt(a)
+
+function changeAnime() {
+    let slctAnime = document.getElementById("select-anime").selectedOptions[0];
+    player.animeName = slctAnime.innerText;
+}
+
+function changeCharacter() {
+    let slctCharacter = document.getElementById("select-cards").selectedOptions[0];
+    player.maxCharacters = parseInt(slctCharacter.value)
+    refreshViewSelector();
+}
+
+function showMyCharacter() {
+    document.getElementById("mycharacter-name").innerText = player.myCharacter.name;
+    document.getElementById("mycharacter-image").alt = player.myCharacter.name;
+    document.getElementById("mycharacter-image").src = player.myCharacter.image;
+
 }
