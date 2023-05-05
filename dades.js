2744a66c880d9d880d377eeba8b74276e59c0c2b
dev <44408822+zackproject@users.noreply.github.com>
2023-05-05 05:14:02 +0200

    Home page (#36)
    * Fix 'quien-es-quien' (#30)
    
    * FIX limit cards
    
    * Create CNAME
    
    * home page

--- dades.js
+++ dades.js
@@ -1,51 +1,51 @@
 var cards = [
     {
         "title": "Piano",
-        "image": "",
+        "image": "https://i.imgur.com/k9tLfsE.png",
         "alt": "Card Anime Birthday",
         "description": "Toca el piano y crea canciones donde otra persona tenga que adivinar tu canción a través de un link.",
-        "link": "/piano"
+        "link": "piano"
     },
     {
         "title": "Quien es Quien",
         "image": "https://i.imgur.com/5uzjtaD.png",
         "alt": "Card quien es quien",
         "description": "Juega en dos dispositivos. Elige una serie, un personaje y juega con tu amigo a adivinar el personaje del otro.",
-        "link": "/quien-es-quien"
+        "link": "quien-es-quien"
     },
     {
         "title": "PokeHigher or Pokelower",
         "image": "https://i.imgur.com/WeM1zLU.png",
         "alt": "Card PokeHigher or PokeLower",
         "description": "Quien tiene mas ataque, defensa o velocidad entre estas dos poke-criaturas.",
-        "link": "/higherlower"
+        "link": "higherlower"
     },
     {
         "title": "Poke Sort",
         "image": "https://imgur.com/oCLFH4j.png",
         "alt": "Card quien poke Sort",
         "description": "Ordena las letras de la poke-criatura detras de la sombra. Puedes elegir cualquier generacion",
-        "link": "/sort"
+        "link": "sort"
     },
     {
         "title": "Poke Guess",
         "image": "https://i.imgur.com/ieg9MPg.png",
         "alt": "Card Poke guess",
         "description": "Estas pokecriaturas estan un poco peladas, adivina de quien se trata. Elige cualquier generacion",
-        "link": "/guess"
+        "link": "guess"
     },
     {
         "title": "Calendario Navidad",
         "image": "https://i.imgur.com/XbRvtk3.png",
         "alt": "Card Calendario Adviento",
         "description": "Cada dia se desbloquea una casilla. Acierta la quiz y consigue la sorpresa.Sólo disponible en diciembre",
-        "link": "/navidad"
+        "link": "navidad"
     },
     {
         "title": "Anime Birthday",
         "image": "https://i.imgur.com/W6FuNtS.png",
         "alt": "Card Anime Birthday",
         "description": "Find the birthday of the character by day and month. Demo of the App Anime Birthday",
-        "link": "/animebirthday"
+        "link": "animebirthday"
     }
 ];
