b025e005719660e91f764bf89c10067097f59d43
zackproject <zackprojectapp@gmail.com>
2023-05-15 01:02:38 +0200

    panel position relative & absolute

--- se-busca/index.js
+++ se-busca/index.js
@@ -24,19 +24,22 @@ class Wanted {
         this.time = time;
         this.points = points;
         this.characterList = characterList;
-        this.myCharacter = characterList[this.randNum(0, characterList.length - 1)];
-        this.panelList = this.getRandPanel(characterList);
+        //S'omplen una vegada creat el panell, getRandCharacter(), getRandPanel()
+        this.myCharacter = null;
+        this.panelList = null;
     }
 
+    getRandCharacter() {
+        this.myCharacter = this.characterList[this.randNum(0, this.characterList.length - 1)]
+    }
 
-
-    getRandPanel(list) {
+    getRandPanel() {
         let i = 0;
         let newLlista = [];
         while (i < this.numberOfCharacters) {
             //Per tots el personatges disponibles, agafa un character random
-            let rand = this.randNum(0, list.length - 1);
-            let randCharacter = list[rand];
+            let rand = this.randNum(0, this.characterList.length - 1);
+            let randCharacter = this.characterList[rand];
             //Si el que busca no es el triat, el fica, el nostre ha de ser unic
             if (randCharacter !== this.myCharacter) {
                 newLlista.push(randCharacter)
@@ -48,30 +51,79 @@ class Wanted {
         let novaUbicacio = this.randNum(0, this.numberOfCharacters - 1)
         //Al acaba sobrescriu un a l'atzar per el nostre personatge
         newLlista[novaUbicacio] = this.myCharacter;
-        return newLlista;
+        this.panelList = newLlista;
     }
 
-    randNum(min, max) {
+    randNum(min, max, double = false) {
+        if (double) {
+            //Ho talla a dos decimals i acaba sent float de 2 decimals
+            return parseFloat((Math.random() * (max - min + 1) + min).toFixed(2));
+        }
         return parseInt(Math.random() * (max - min + 1) + min);
     }
 
-
 }
-let a = new Wanted(0, sebusca, 60, 100);
+
+let player = new Wanted(0, sebusca, 60, 30);
+//Genera el nou character
+player.getRandCharacter();
+//Genera el nou panell
+player.getRandPanel();
 
 function dibujaenGrid() {
     let pare = document.getElementById("panelGrid");
-    for (let i = 0; i < a.panelList.length; i++) {
+    //Esborra el contigut de l'anterior
+    deleteChilds(pare);
+    //Torna a generar la partida
+    player.getRandCharacter();
+    player.getRandPanel();
+    //Fica el personatge triat
+    document.getElementById("myCharacter").src = player.myCharacter.image;
+    for (let i = 0; i < player.panelList.length; i++) {
         let img = document.createElement("img");
-        img.src = a.panelList[i].image;
+        img.src = player.panelList[i].image;
         img.width = "100"
-        img.alt = a.panelList[i].name;
+        img.alt = player.panelList[i].name;
         pare.appendChild(img)
     }
-    document.getElementById("myCharacter").src = a.myCharacter.image;
+}
+
+
+
+function dibujaenPosition() {
+    let pare = document.getElementById("panelAbsoluto");
+    //Esborra el contigut de l'anterior
+    deleteChilds(pare);
+    //Torna a generar la partida
+    player.getRandCharacter();
+    player.getRandPanel();
+    player.panelList.forEach(element => {
+        let img = document.createElement("img");
+        img.src = element.image;
+        img.width = "100"
+        img.alt = element.name;
+        // Si el pare es relative la imatge es absoluta
+        img.style.position = "absolute";
+        // Tria una posicio x y numero random 'double' dins del panell. Al 100% no es veu
+        img.style.left = player.randNum(0, 90, true) + "%";
+        img.style.top = player.randNum(0, 90, true) + "%";
+        //El personatge triat ha d'estar sobre per poder clicarlo
+        if (element === player.myCharacter) img.style.zIndex = 1;
+        // Puja la imatge en el container
+        pare.appendChild(img)
+    });
+    for (let i = 0; i < player.panelList.length; i++) {
+
+    }
+    document.getElementById("myCharacter").src = player.myCharacter.image;
+    console.log("pedro");
 }
 
 
 
 
-
+function deleteChilds(currentDiv) {
+    while (currentDiv.firstChild) {
+        currentDiv.removeChild(currentDiv.firstChild);
+    }
+}
