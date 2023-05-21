6e3497aa621c9b3ba23a4c22d65ad949fa9e0104
zackproject <zackprojectapp@gmail.com>
2023-05-21 23:03:16 +0200

    Animation and update ronda with button

--- se-busca/index.js
+++ se-busca/index.js
@@ -59,6 +59,9 @@ let maxCharacters = 4;
 //Controla si es 0-flex, 1-position, 2-staticPosition 3-table
 let typePanel = 0;
 
+//Controla totes les imatges de la partida
+const IMGINGAME = "img-in-game";
+//Crea la partida
 let player = new Wanted(points, sebusca, timer, maxCharacters, typePanel);
 //Genera el nou character
 player.getRandCharacter();
@@ -84,8 +87,10 @@ function drawPanelFlex() {
         img.src = player.panelList[i].image;
         img.width = "100"
         img.alt = player.panelList[i].name;
+        //Controla quien elements ha d'ocultar per clase
+        img.className = IMGINGAME;
         //Quan cliques una imatge s'envia la seva data a la funcio 'getInfoClicked'
-        img.addEventListener("click", () => { getInfoClicked(player.panelList[i]) });
+        img.addEventListener("click", (event) => { getInfoClicked(player.panelList[i], event) });
         pare.appendChild(img)
     }
 }
@@ -93,10 +98,17 @@ function drawPanelFlex() {
 
 //PANEL RELATIVE/ABSOLUTE
 function drawPanelPosition() {
+    //Tria si fara l'animacio
+    let faAnimacio = player.randNum(0, 3) === 1;
+    console.log("animas???", faAnimacio);
+
     document.getElementById("typePanel").innerText = " en Position";
     let pare = document.getElementById("panelAbsoluto");
     //Esborra el contigut de l'anterior
     deleteChilds(pare);
+
+    pare.innerHTML = "<button id='btn-next-partida' class='center-next-btn' onclick='nextPartida()''>SIGUIENTE</button>"
+
     //Torna a generar la partida
     player.typePanel = 1;
     player.getRandCharacter();
@@ -106,14 +118,21 @@ function drawPanelPosition() {
         img.src = element.image;
         img.width = "70"
         img.alt = element.name;
+        //Controla quien elements ha d'ocultar per clase
+        img.className = IMGINGAME;
         // Si el pare es relative la imatge es absoluta
         img.style.position = "absolute";
         // Tria una posicio x y numero random 'double' dins del panell. Al 100% no es veu
         img.style.left = player.randNum(0, 90, true) + "%";
         img.style.top = player.randNum(0, 90, true) + "%";
-        if (i % 2 == 0 && i != 0) img.style.animationDirection = "reverse";
+        if (i % 2 == 0 && i != 0) img.style.animationDirection = "normal";
+        if (i % 2 != 0 && i != 0) img.style.animationDirection = "reverse";
+
         else img.style.animationDirection = "normal";
-        //img.style.animationDelay = i/10 + "s";
+        //Dependen de la puntuacio i de l'atzar aplica animacio
+        if (faAnimacio) {
+            applyAnimation(img);
+        }
         //El personatge triat ha d'estar sobre per poder clicarlo
         if (element === player.myCharacter) {
             img.style.zIndex = 2;
@@ -121,9 +140,8 @@ function drawPanelPosition() {
         else {
             img.style.zIndex = 1;
         }
-        console.log("i", i);
         //Quan cliques una imatge s'envia la seva data a la funcio 'getInfoClicked'
-        img.addEventListener("click", () => { getInfoClicked(element) });
+        img.addEventListener("click", (event) => { getInfoClicked(element, event) });
         // Puja la imatge en el container
         pare.appendChild(img)
     });
@@ -135,9 +153,11 @@ function drawPanelPosition() {
 //PANEL RELATIVE/ABSOLUTE 4 ELEMENTS
 function drawPanelStaticPosition() {
     document.getElementById("typePanel").innerText = " en Static";
-    let pare = document.getElementById("panelStatico");
+    let pare = document.getElementById("panelAbsoluto");
     //Esborra el contigut de l'anterior
     deleteChilds(pare);
+    pare.innerHTML = "<button id='btn-next-partida' class='center-next-btn' onclick='nextPartida()''>SIGUIENTE</button>"
+
     //Com nomes en aquest panel necesito 4, faig copia i aplico
     let copiaNumber = player.numberOfCharacters;
     player.numberOfCharacters = 4;
@@ -151,16 +171,18 @@ function drawPanelStaticPosition() {
         img.src = element.image;
         img.width = "100"
         img.alt = element.name;
+        //Controla quien elements ha d'ocultar per clase
+        img.className = IMGINGAME;
         // Si el pare es relative la imatge es absoluta
         img.style.position = "absolute";
         // Tria una posicio x y numero random 'double' dins del panell. Al 100% no es veu
         img.style.left = tamany * 17 + "%";
-        img.style.top = "-50%";
+        img.style.top = "-10%";
         tamany++;
         //El personatge triat ha d'estar sobre per poder clicarlo
         if (element === player.myCharacter) img.style.zIndex = 1;
         //Quan cliques una imatge s'envia la seva data a la funcio 'getInfoClicked'
-        img.addEventListener("click", () => { getInfoClicked(element) });
+        img.addEventListener("click", (event) => { getInfoClicked(element, event) });
         // Puja la imatge en el container
         pare.appendChild(img)
     });
@@ -173,19 +195,18 @@ function drawPanelStaticPosition() {
 }
 
 function drawPanelTable() {
-
-    let posTr = 0;
-    let posTd = 0;
+    //let posTr = 0;
+    //let posTd = 0;
     document.getElementById("typePanel").innerText = " en Tabla";
     let pare = document.getElementById("panelTabla");
 
     deleteChilds(pare);
-    player.typePanel = 4;
+    player.typePanel = 3;
     player.getRandCharacter();
     player.getRandPanel();
 
     let table = document.createElement("table");
-    let numColumnes = 2;
+    let numColumnes = player.randNum(2, 3);
 
     let tdrestant = numColumnes - (player.numberOfCharacters % numColumnes);
     console.log(tdrestant);
@@ -201,7 +222,12 @@ function drawPanelTable() {
     let tr = document.createElement("tr");
     for (let i = 0; i < player.numberOfCharacters; i++) {
         let td = document.createElement("td")
-        td.innerText = player.panelList[i].name;
+        //Creacio buto
+        let btn = document.createElement("button");
+        btn.innerText = player.panelList[i].name;
+        btn.addEventListener("click", (event) => { getInfoClicked(player.panelList[i], event) });
+        //Fica als fills
+        td.appendChild(btn);
         tr.appendChild(td)
         table.appendChild(tr)
         //En cas que sigui divisible entre el numde columnes, fa una final nova
@@ -217,10 +243,11 @@ function drawPanelTable() {
 
 
     let h3 = document.createElement("h3");
-    h3.innerText = "Se busca a " + player.myCharacter.name + " | Fila: " + posTr + " Columna: " + posTd;
+    h3.innerText = "Se busca a " + player.myCharacter.name; //+ " | Fila: " + posTr + " Columna: " + posTd
     pare.appendChild(h3)
     pare.appendChild(table)
 }
+
 
 //Mostra el personatge que has de buscar al HTML
 function showHTMLMyCharacter(nPlayer) {
@@ -236,29 +263,101 @@ function deleteChilds(currentDiv) {
     }
 }
 
+let pepe = null;
 
 //Funcio que aplica quan cliques a una imatge
-function getInfoClicked(props) {
+function getInfoClicked(props, event) {
     if (player.myCharacter === props) {
         console.log("Correcto", props);
         //Puja el nivell
         player.updatePanel();
-        console.log("Points:", player.points, "Panel:", player.panelList.length);
-        switch (player.typePanel) {
-            case 0:
-                drawPanelFlex()
-                break;
-            case 1:
-                drawPanelPosition();
-                break;
-            case 2:
-                drawPanelStaticPosition();
-            default:
-                console.log("Tipus de panell no definit");
-                break;
-        }
+        //Si es correcte la pantalla canviara
+        ocultaIncorrectes();
     } else {
-        console.log("Ups, no era", props.name);
+        console.log("Ups, noera", props.name);
     }
     console.log(props.name);
+
+    //event.target.disabled = true;
+    //console.log(event.target.disabled);
+    pepe = event.target;
+    console.log("pepe tiene valor", pepe);
+    //event.target.removeEventListener('click', getInfoClicked);
+    //let eventsClicable = getEventListeners(event.target).click[0];
+    //Esborra el primer element clicable, os sigui a getInfoClicked
+    //event.target.removeEventListener(eventsClicable.type, eventsClicable.listener);
+}
+
+function nextPartida() {
+    console.log("Points:", player.points, "Panel:", player.panelList.length);
+
+    if (player.typePanel === 1 && player.points < 10) {
+        player.typePanel = player.randNum(1, 2);
+    } else {
+        player.typePanel = 1;
+    }
+    console.log("elegi panel", player.typePanel);
+
+    switch (player.typePanel) {
+        case 0:
+            drawPanelFlex()
+            break;
+        case 1:
+            drawPanelPosition();
+            break;
+        case 2:
+            console.log("que guay");
+            drawPanelStaticPosition();
+            break;
+        case 3:
+            drawPanelTable();
+            break;
+        default:
+            console.log("Tipus de panell no definit: " + player.typePanel);
+            break;
+    }
+    document.getElementById("btn-next-partida").style.display = "none";
+}
+
+function ocultaIncorrectes() {
+    let elements = document.getElementsByClassName(IMGINGAME);
+    for (let i = 0; i < elements.length; i++) {
+        //Si no es el meu personatge, oculta les imatges
+        if (elements[i].alt !== player.myCharacter.name) {
+            elements[i].style.display = "none";
+        }
+    }
+    //Mostra el boto de seguent
+    document.getElementById("btn-next-partida").style.display = "block";
+}
+
+
+//Controla les animacions que nomes s'aplican a l'atzar si es major 10 la puntuacio
+function applyAnimation(element) {
+    if (player.points > 10) {
+        //Si ,es de 10 punts, dos animacions, si es 23, 2 animacion 
+        let animacionsDisponibles = player.points % 10;
+        //No deixa que hi hagi mes punts que animacions
+        //EX: 53 punts no hi ha animacio '5'
+        if (animacionsDisponibles > 3) { animacionsDisponibles = 3; }
+        //Tria animacions random per cada imatge, poden repetirse en el bucle
+        let num = player.randNum(0, animacionsDisponibles);
+        console.log("randimation", num,"max",animacionsDisponibles);
+        //Tria una animacio
+        switch (num) {
+            case 0:
+                element.style.animationName = "moveFromY";
+                break;
+            case 1:
+                element.style.animationName = "moveFromX";
+                break
+            case 2:
+                element.style.animationName = "circle";
+                break;
+            default: //3
+                element.style.animationName = "rotateDiagonal";
+                break;
+        }
+        console.log("aniamcion", element.style.animationName);
+    }
 }
