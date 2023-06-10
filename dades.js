4712a90c17ea2b850dd0d479e41a5f009c66b5fd
zackproject <zackprojectapp@gmail.com>
2023-06-10 20:03:31 +0200

    text como jugar

--- dades.js
+++ dades.js
@@ -34,14 +34,14 @@ var cards = [
         "alt": "Card quien poke Sort",
         "description": "Ordena las letras de la poke-criatura detras de la sombra. Puedes elegir cualquier generacion",
         "link": "sort"
-    },
+    },/*
     {
         "title": "Poke Guess",
         "image": "https://i.imgur.com/ieg9MPg.png",
         "alt": "Card Poke guess",
         "description": "Estas pokecriaturas estan un poco peladas, adivina de quien se trata. Elige cualquier generacion",
         "link": "guess"
-    },
+    },*/
     {
         "title": "Calendario Navidad",
         "image": "https://i.imgur.com/XbRvtk3.png",
