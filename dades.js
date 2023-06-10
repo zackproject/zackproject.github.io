9e70ae01a25f80dd7319e77854d5682c5c026bde
dev <44408822+zackproject@users.noreply.github.com>
2023-06-10 20:41:44 +0200

    Fix dev (#56)
    * text como jugar
    
    * footer

--- dades.js
+++ dades.js
@@ -22,6 +22,13 @@ var cards = [
         "link": "quien-es-quien"
     },
     {
+        "title": "Calendario Navidad",
+        "image": "https://i.imgur.com/XbRvtk3.png",
+        "alt": "Card Calendario Adviento",
+        "description": "Cada dia se desbloquea una casilla. Acierta la quiz y consigue la sorpresa.Sólo disponible en diciembre",
+        "link": "navidad"
+    },
+    {
         "title": "PokeHigher or Pokelower",
         "image": "https://i.imgur.com/WeM1zLU.png",
         "alt": "Card PokeHigher or PokeLower",
@@ -34,21 +41,14 @@ var cards = [
         "alt": "Card quien poke Sort",
         "description": "Ordena las letras de la poke-criatura detras de la sombra. Puedes elegir cualquier generacion",
         "link": "sort"
-    },
+    },/*
     {
         "title": "Poke Guess",
         "image": "https://i.imgur.com/ieg9MPg.png",
         "alt": "Card Poke guess",
         "description": "Estas pokecriaturas estan un poco peladas, adivina de quien se trata. Elige cualquier generacion",
         "link": "guess"
-    },
-    {
-        "title": "Calendario Navidad",
-        "image": "https://i.imgur.com/XbRvtk3.png",
-        "alt": "Card Calendario Adviento",
-        "description": "Cada dia se desbloquea una casilla. Acierta la quiz y consigue la sorpresa.Sólo disponible en diciembre",
-        "link": "navidad"
-    },
+    },*/
     {
         "title": "Anime Birthday",
         "image": "https://i.imgur.com/W6FuNtS.png",
