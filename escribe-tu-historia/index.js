368e6cf08394959fb1582822ce497d3059b586e6
zackproject <zackprojectapp@gmail.com>
2023-07-10 20:55:19 +0200

    New Story

---
+++ escribe-tu-historia/index.js
@@ -0,0 +1,166 @@
+let catala = ["Vegada", "Cotxe", "Maons", "Regar", "Coca", "Prendre", "Pesat", "Ganivet", "Digital",
+    "Confidencial", "Premi", "Repetir", "Mitjó", "Batre", "Somiar", "Ciclomotor", "Agitar",
+    "Enemic", "Remolcador", "Criatura", "Llum", "Igual", "Cuir", "Sortida", "Secret", "Soroll",
+    "Aigua", "Formiguer", "Espectre", "Por", "Especial", "Freda", "Tot", "Galeta", "Descans",
+    "Extractor", "Xeringa", "Terres", "Paperera", "Rentar", "Infern", "Descongelar", "Planxar",
+    "Progenitor", "Mal", "Disfressa", "Rodar", "Bombeta", "Cadira", "Excavar", "Escenari",
+    "Consulta", "Xoc", "Duel", "Joia", "Engany", "Remenar", "Fracassar", "Volar", "Teclat",
+    "Bebible", "Foradar", "Llaminadura", "Pur", "Ofensa", "Carnaval", "Quadrat", "Macarrons",
+    "Elegit", "Pesada", "President", "Manicura", "Esbandir", "Assecar", "Geni", "Vals",
+    "Volum", "Orifici", "Companyia", "Filosofia", "Nadó", "Pal", "Ajuda", "Entumit",
+    "Lladruc", "Embussar", "Mentir", "Segon", "Enfosquir", "Assignatura", "Rodanxa",
+    "Borratxo", "Traductor", "Podi", "Avions", "Gavina", "Assassinar", "Agut", "Casament",
+    "Gaudir", "Fantasma", "On", "Mosquit", "Toves", "Arbustos", "Recepta", "Harmonia",
+    "Eleccions", "Llavor", "Grau", "Natural", "Genollera", "Llanda", "Barrejar",
+    "Interruptor", "Empalmar", "Gong", "Professora", "Irritar", "Llagosta", "Kamikaze",
+    "Trencar", "Remera", "Blau", "Porter", "Adormit", "Vies", "Amanida", "Garlanda",
+    "Verge", "Diagonal", "Mil", "Antiguitat", "Fina", "Muralla", "Butlleta", "Ràbia", "Sot",
+    "Cacau", "Velòdrom", "Dibuix", "Gruix", "Rosat", "Resultat", "Agafar", "Mitgera", "Cargol",
+    "Abelles", "Trencat", "Silicona", "Gola", "Guineu", "Etapes", "Aposta", "Cartera", "Fregides",
+    "Gat", "Temps", "Conyac", "Retrat", "Nom", "Ou", "Fracassar", "Sempre", "Pica", "Llegir", "Fregir",
+    "Alcohol", "Inesperada", "Barretina", "Urgent", "Vall", "Tub", "Tigre", "Vermell", "Aixecament",
+    "Esquí", "Blancaneus", "Fleca", "Et", "Esperons", "Circuit", "Servir", "Avergonyir", "Surf",
+    "Mapamundi", "Quadres", "Holanda", "Eruga", "Balena", "Visitar", "Peu", "Calamar", "Alicates",
+    "Avis", "Eina", "Gripau", "Patins", "Plat", "Cistella", "Miratge", "Crani", "Aprimar",
+    "Rialla", "Costerut", "Espelmes", "Cames", "Empatar", "Paracaigudisme", "Policia", "Blanques",
+    "Segons", "Conduir", "Germans", "Camell", "Carruatge", "Decorar", "Tirallet", "Ressonar",
+    "Consultori", "Disputa", "Delicte", "Clau", "Faldilla", "Cinc", "Tornar", "Ficat",
+    "Llac", "Camell", "Acomodador", "Pluja", "Genet", "Papagai", "Cors", "Taxi", "Cordes", "Nu",
+    "Mico", "Pileta", "Coctelera", "Sorteig", "Agitar", "Triturar", "Prendre", "Enemic",
+    "Pinzell", "Ocell", "Rastre", "Canals", "Anomenada", "Descobridor", "Yeti", "Granger",
+    "Pruna", "Gira-sol", "Guanyador", "Esculpir", "Sempre", "Treball", "Veritat", "Farcit",
+    "Xinxa", "Gronxador", "Respirar", "Projectar", "Cadell", "Costerut", "Art", "Pop", "Joves",
+    "Parlar", "Aquest", "Regals", "Veterinari", "Punyal", "Colesterol", "Lents", "Anomenar",
+    "Imantada", "Megàfon", "Criticar", "Estrelles", "Quinze", "Creixent", "Ample", "Prop", "Definició",
+    "Avui", "Esgrima", "Creure", "Esquerra", "Finestra", "Vidriera", "Refugi", "Vibrar", "Llimac",
+    "Cometes", "Cendres", "Adult", "Opinió", "Inspeccionar", "Salt", "Acord", "Fama", "Domesticar",
+    "Esportiu", "Espatlla", "Gall", "Xemeneia", "Cendra", "Duana", "Mesos", "Xiuxiueig", "Abordatge",
+    "Caspa", "Censura", "Contraban", "Insensible", "Invertir", "Dalt", "Misteri", "Molí De Vent", "Bolquer",
+    "Sempre", "Aperitiu", "Artista", "Cordar", "Filtre", "Loteria", "Continuar", "Filferro", "Científica",
+    "Publicitat", "Llenyataire", "Prim", "Canonada", "Escates", "Avantpassat", "Fracassar", "Maduresa",
+    "Mesquita", "Rodanxa", "Venir", "Sostre", "Radical", "Farcit", "Capità", "Tatuatge", "Protesta", "Lladre",
+    "Avi", "Loteria", "Conquerir", "Mel", "Esperit", "Missatge", "Casset", "Dinosaure", "Passar", "Pensament",
+    "Cosir", "Estada", "Numerar", "Ull", "Investigador", "Grua"];
+
+let castellano = ["vez", "látigo", "fruta", "oveja", "pulgada", "carta", "palo", "socio", "cena", "jauría", "cajón",
+    "mano", "ropa interior", "nivel", "federación", "manos", "roca", "trabajo", "juguete", "billete", "pregunta",
+    "nación", "ira", "cuenta", "conejero", "living", "acción", "rayo", "bate", "departamento", "corbata", "descanso",
+    "cuadrado", "burro", "techo", "serie", "tapa", "pulgada", "pestaña", "factura", "enciclopedia", "crimen", "banco",
+    "palos", "labio", "aljaba", "debate", "brasileño", "palo", "estofado", "recipiente", "pantalón", "barco", "brazo",
+    "acero", "almeja", "monitor", "azúcar", "trenes", "mostrar", "vástago", "polvo", "minuto", "guitarra", "patada",
+    "hecho", "niebla", "rastrillo", "rúcula", "furgoneta", "cuenca", "mensaje", "mariquita", "madera", "conexión",
+    "céfiro", "cara", "angustia", "talla", "río", "cable", "cuarto", "faro", "cebra", "mañana", "intercambio", "colonia",
+    "juguete", "empezar", "micrófono", "aves de corral", "columpio", "luna", "ramita", "curva", "acústica", "bicicleta",
+    "mermelada", "ojos", "distancia", "hermano", "castillo", "sangre", "cebo", "fiesta", "jarrón", "hogar", "suciedad",
+    "cohete", "ordenar", "miedo", "acertijo", "oro", "zapatos", "entrenador", "candidato", "teatro", "asiático", "cena",
+    "micrófono", "rúcula", "playa", "silbato", "piernas", "madera", "carámbano", "tirar de", "acera", "río", "libros",
+    "invento", "amigos", "transformador", "colina", "doblar", "animal", "león", "rodar", "transporte", "estación", "criada",
+    "autoridad", "universidad", "satélite", "cumpleaños", "tarro", "necesita", "tecla", "hija", "ponche de huevo", "vena",
+    "dormir", "cara", "concretos", "holandés", "hora", "día", "oso", "remate", "acústica", "encaje", "empujar", "pueblo",
+    "formar", "agarre", "brasileño", "vitalidad", "peso", "conducir", "inteligencia", "alambre", "cortina", "equipo",
+    "moneda", "lentes", "conexión", "nuez", "traje", "pobreza", "pasado", "mermelada", "pingüino", "extraño", "cortina",
+    "olla", "furgoneta", "rodar", "tos", "marco", "lectura", "huevo", "alumno", "agenda", "mármol", "actual", "girar",
+    "reventar", "patín", "telaraña", "logro", "pera", "pantalla", "lluvia", "bombilla", "gatitos", "auto", "pala", "estufa",
+    "percha", "freno", "parte bajo", "cazuela", "cuenta", "secuelas", "negocio", "protesta", "anteojos", "computadora",
+    "parabrisas", "alameda", "mapa", "oveja", "cepillo de dientes", "historia", "toro", "paraguas", "respeto", "arañas",
+    "cinturón", "robin", "lino", "propiedad", "colegio", "yak", "lapicera", "constelación", "edad", "caramelos", "servilleta",
+    "periodista", "ejército", "irakí", "proyector", "sentimiento", "partido", "plástico", "piso", "abeja", "plantas",
+    "corte de pelo", "rodilla", "máscara", "queso", "bomba", "percha", "llave", "calle", "estadística", "vegetal", "zapato",
+    "cerdo", "yugo", "ángulo", "cebo", "oportunidad", "beso", "juguete", "descanso", "campanas", "información", "cebo",
+    "cementerio", "crío", "insignia", "bombilla", "luces", "ardilla", "mermelada", "comportamiento", "acuerdo", "trono",
+    "música", "rodar", "valor", "competencia", "flor", "pluma", "líquido", "jefe", "curva", "espiar", "intercambio", "grano",
+    "aplastar", "declaración", "pago", "abuela", "irakí", "beber", "fundición", "parque infantil", "quitasol", "expansión",
+    "horno", "sugerencia", "movimiento", "recipiente", "acolchado", "enseñar", "tela", "pájaros", "perro", "memoria", "utilizar",
+    "federación", "presidente", "parte", "partido", "alambre", "agujero", "tornillo", "combustible", "patata", "papá", "baño",
+    "tranquilo", "vacaciones", "terremoto", "diputado", "gobernador", "papá", "nido", "diseño", "conocimiento", "pantalones",
+    "viaje", "menta", "oro", "acero", "instrumento", "árbol", "partido", "mamá", "cobre", "arbustos", "viaje", "jalea", "enciclopedia",
+    "camisa", "teclado", "abuela", "gusano", "gorra", "suéter", "patata", "correr", "oso", "pantalón", "soporte", "teléfono", "elemento",
+    "discusión", "medida", "cazuela", "cartas"];
+
+let story = [
+    {
+        "id": 1,
+        "title": "El gato",
+        "parrafs": [
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+        ]
+    },
+    {
+        "id": 2,
+        "title": "Mas alla de la luz",
+        "parrafs": [
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+        ]
+    },
+    {
+        "id": 3,
+        "title": "El gato de Pedro",
+        "parrafs": [
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur mollitia maiores laudantium enim",
+        ]
+    },
+];
+
+class Story {
+    constructor(title, Chapter) {
+        this.title = title;
+        this.Chapter = Chapter;
+    }
+}
+
+class Chapter {
+    constructor(id, title, parrafs) {
+        this.id = id;
+        this.title = title;
+        this.parrafs = parrafs;
+    }
+}
+
+let pare = document.getElementById("story");
+
+let maxParrafsByChapter = 7;
+
+story.forEach(element => {
+    let title = element.id + " - " + element.title;
+    let cap = createHTMLChapter(title, element.parrafs);
+    console.log("cat", cap);
+    pare.appendChild(cap);
+    console.log(element);
+});
+function getAreaText() {
+    let getTextArea = document.getElementById("story-area").value;
+    console.log(getTextArea.length);
+}
+
+function createHTMLChapter(title, parrafs) {
+    console.log("pedro", parrafs);
+    let summary = document.createElement("summary");
+    summary.innerText = "Capitulo " + title;
+    let detail = document.createElement("details");
+    detail.appendChild(summary);
+    parrafs.forEach(element => {
+        let p = document.createElement("p");
+        p.innerText = element;
+        p.className = "p-day";
+        detail.appendChild(p);
+    });
+    return detail;
+}
