0a86461edc1998a72f8d793a2534f17606ba77a9
zackproject <zackprojectapp@gmail.com>
2023-08-11 20:41:21 +0200

    Guess song and next

--- adivina-anime/index.js
+++ adivina-anime/index.js
@@ -3,47 +3,96 @@
 
 
 
-// elige una cancion  de una lista desordenada
-let song = new Audio(playlist[0].fragment);
-// no timeout, reveal name, repeat
+// elige una cancion  de una lista desordenada+
+let iSong = 26;
+let songOBJ = playlist[iSong];
+let song = new Audio(songOBJ.fragment);
 let btnGuessAnime = document.getElementById("btn-play-anime");
+let animeSolution = document.getElementById("anime-solution");
+
 let progressbar = document.getElementById("progress-song");
 const intervalID = null;
+let timeToResolve = 10;
+let number = 0; //0, 1,2,3 // 3 all-audio
+fillSolution(songOBJ);
+function fillSolution({ title, fragment, song, author }) {
+    document.getElementById("title-anime").innerText = title;
+    let imgAnime = document.getElementById("img-anime");
+    imgAnime.src = `http://i3.ytimg.com/vi/${song.split("/").pop()}/hqdefault.jpg`
+    imgAnime.alt = `Miniatura del video de YouTube ${title} del autor ${author}`
+    let authorAnime = document.getElementById("author-anime");
+    authorAnime.innerText = "Canción de " + author;
+    authorAnime.href = song;
+}
 function pressBtn() {
     if (!song.paused) {
-        console.log("Tienes 20 segundos para adivinar");
         song.pause();
+        song.currentTime = 0;
+        btnGuessAnime.innerText = "electric_bolt";
+        btnGuessAnime.disabled = true;
+        callSolution();
         return;
     } else {
+        if (number < 3) number++;
+        song.currentTime = 0;
+        btnGuessAnime.stu
         playSong();
-        btnGuessAnime.innerText = "back_hand";
-        btnGuessAnime.title ="Adivinar";
+        fillSolution(songOBJ);
+        animeSolution.style.display = "none";
+        if (number != 3) {
+            timeOut(number);
+        }
+        btnGuessAnime.innerText = "touch_app";
+        btnGuessAnime.style.backgroundColor = "red"
+        btnGuessAnime.title = "Adivinar";
     }
-
-
-
-
 }
 
+function callSolution() {
+    let time = timeToResolve;
+    let countDownHTML = document.getElementById("countdown");
+    countDownHTML.innerText = time;
+    const intervalID = setInterval(() => {
+        if (time > 0) {
+            time--;
+            console.log("tiempo", time);
+            countDownHTML.innerText = time;
+
+        } else {
+            clearInterval(intervalID);
+            countDownHTML.innerText = "";
+            animeSolution.style.display = "flex";
+            btnGuessAnime.disabled = false;
+            btnGuessAnime.innerText = "play_arrow";
+            btnGuessAnime.style.backgroundColor = "green"
+            iSong++;
+            number = 0;
+            songOBJ = playlist[iSong];
+            song = new Audio(songOBJ.fragment);
+        }
+        console.log();
+    }, 1000);
+}
 function playSong() {
     //Primera vez interval 5 segundos, segunda vez, interval 10, tercera vez sin interval
     const intervalID = setInterval(() => {
         let calcul = song.currentTime / song.duration * 100;
         progressbar.style.width = calcul + "%";
     }, 500);
-
     song.play();
+}
+
+function timeOut(number) {
     setTimeout(function () {
         clearInterval(intervalID);
         song.pause();
-        song.currentTime = 0;
         // Si pulsan el boton mientras esta el repetir, cuenta atras para decir el titulo
         // sino aparece solo
         btnGuessAnime.innerText = "replay";
-        btnGuessAnime.title ="Repetir";
+        btnGuessAnime.style.backgroundColor = "green"
+        btnGuessAnime.title = "Repetir";
 
-    }, 5000);
-
+    }, number * 5000);
 
 }
 
@@ -61,7 +110,7 @@ function template(props) {
             Your browser does not support the audio element.
         </audio> */
 }
-let pare = document.getElementById("container");
+/*let pare = document.getElementById("container");
 let llista = playlist.sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()));
 let total = 0;
 llista.forEach((element) => {
@@ -70,4 +119,4 @@ llista.forEach((element) => {
         total++;
     }
 });
-document.getElementById("total").innerText = total;
+document.getElementById("total").innerText = total;*/
