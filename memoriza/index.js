cfbddf4941a4545770e791375c5e0f23775da7c1
zackproject <zackprojectapp@gmail.com>
2024-10-09 03:58:47 +0200

    Class Memorize + selector speed

--- memoriza/index.js
+++ memoriza/index.js
@@ -1,39 +1,103 @@
-const speed = 2000;
-const btn = document.getElementById("infoBtn");
-const btnList = document.getElementsByClassName("btn-game");
-const infoBtn = document.getElementById("infoBtn");
-let cont = 0;
-let responseList = [];
-btn.innerText = "INICIAR";
-// add funcions to button
-for (let i = 0; i < btnList.length; i++) {
-  btnList[i].addEventListener("click", () => {
-    // send 0,1,2,3 in sendBtn
-    sendBtn(i);
-  });
+class Memorize {
+  constructor(speed) {
+    this.sequenceList = [];
+    this.cont = 0;
+    // miliseconds
+    this.speed = speed;
+  }
+
+  addSequence() {
+    // 4 buttons avaliables: [0, 1, 2, 3]
+    this.sequenceList.push(randInt(0, 3));
+  }
+  getSpeed() {
+    return this.speed;
+  }
+  getDelay(i) {
+    // delay between animation
+    return this.speed * i;
+  }
+  getLastDelay() {
+    return this.sequenceList.length * this.speed;
+  }
+
+  getSequenceList() {
+    return this.sequenceList;
+  }
+
+  setSpeed(mSpeed) {
+    this.speed = mSpeed;
+  }
+
+  resetCont() {
+    this.cont = 0;
+  }
+
+  resetGame() {
+    this.resetCont();
+    this.sequenceList = [];
+  }
+
+  addCont() {
+    this.cont++;
+  }
+
+  getPercent() {
+    return ((this.cont + 1) * 100) / this.sequenceList.length;
+  }
+
+  isLastSequence() {
+    return this.cont == this.sequenceList.length;
+  }
+  isCorrectSequence(value) {
+    return value == this.sequenceList[this.cont];
+  }
 }
+
+const circleBtn = document.getElementById("infoBtn");
+const btnList = document.getElementsByClassName("btn-game");
+
+// 2000 Lento
+// 1500 normal
+// 1000 rapido
+// 500 super rapido
+// 200 hiperrapido
+
+let player = new Memorize(1500);
+
+circleBtn.innerText = "INICIAR";
 
 function loadAssets() {
   console.log("Load Page");
+  // add funcions to button
+  for (let i = 0; i < btnList.length; i++) {
+    btnList[i].addEventListener("click", () => {
+      // send 0,1,2,3 in sendBtn
+      sendBtn(i);
+    });
+  }
 }
 
 function playSequence() {
-  infoBtn.style.setProperty("--percentage", 0);
-  responseList.push(randInt(0, 3));
-  btn.innerText = "ATENTO";
-  btn.style.backgroundColor = "rgb(29, 29, 29)";
-  btn.disabled = true;
+  // inizialize new sequence
+  player.addSequence();
+  circleBtn.style.setProperty("--percentage", 0);
+  circleBtn.innerText = "ATENTO";
+  circleBtn.disabled = true;
   useButtons(true);
-  cont = 0;
-  for (let i = 0; i < responseList.length; i++) {
+  player.resetCont();
+  let mSequenceList = player.getSequenceList();
+  // play sequence
+  for (let i = 0; i < mSequenceList.length; i++) {
     setTimeout(() => {
-      visualEffect(responseList[i]);
-    }, i * speed);
+      visualEffect(mSequenceList[i]);
+    }, player.getDelay(i));
   }
+  // after sequence, guess order
   setTimeout(() => {
     useButtons(false);
-    btn.innerText = "TU TURNO";
-  }, responseList.length * speed);
+    circleBtn.innerText = "TU TURNO";
+  }, player.getLastDelay());
 }
 
 function useButtons(isEnabled) {
@@ -42,31 +106,27 @@ function useButtons(isEnabled) {
   }
 }
 
-function sendBtn(value) {
-  if (value == responseList[cont]) {
-    infoBtn.style.setProperty(
-      "--percentage",
-      ((cont + 1) * 100) / responseList.length
-    );
+function sendBtn(mValue) {
+  if (player.isCorrectSequence(mValue)) {
+    circleBtn.style.setProperty("--percentage", player.getPercent());
   } else {
-    btn.innerText = "OTRA VEZ";
-    btn.disabled = false;
-    responseList = [];
-    cont = 0;
+    circleBtn.innerText = "OTRA VEZ";
+    circleBtn.disabled = false;
+    player.resetGame();
   }
-  cont++;
-  if (cont === responseList.length) {
-    btn.innerText = "SEGUIR";
-    btn.style.backgroundColor = "rgb(84, 84, 84)";
-    btn.disabled = false;
+  player.addCont();
+  if (player.isLastSequence()) {
+    circleBtn.innerText = "SEGUIR";
+    circleBtn.style.backgroundColor = "rgb(84, 84, 84)";
+    circleBtn.disabled = false;
   }
 }
 
-function visualEffect(value) {
-  applyCssEffect(btnList[value]);
+function visualEffect(mValue) {
+  applyCssEffect(btnList[mValue]);
   setTimeout(() => {
-    clearCssEffect(btnList[value]);
-  }, 1000);
+    clearCssEffect(btnList[mValue]);
+  }, player.getSpeed() / 2);
 }
 
 function applyCssEffect(mDoc) {
@@ -83,4 +143,8 @@ function clearCssEffect(mDoc) {
 
 function randInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function changeSpeed(event) {
+  player.setSpeed(parseInt(event.target.value));
 }
